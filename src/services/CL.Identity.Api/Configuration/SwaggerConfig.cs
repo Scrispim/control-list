6032a67ee8753ec68ebc8e9d8b269bf56b4ed770
Sander Crispim da Silva <sandercrispim@hotmail.com>
2022-08-22 21:16:41 -0300

    Api Identity

---
+++ src/services/CL.Identity.Api/Configuration/SwaggerConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace CL.Identity.api.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "SC Store Enterprise Identity Api",
+                    Description = "This app it is a study about distribuid application.",
+                    Contact = new OpenApiContact() { Name = "Sander Crispim", Email = "contato@contato.com" },
+                    License = new OpenApiLicense() { Name = "Mit", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
--- src/services/CL.Identity.Api/Startup.cs
+++ src/services/CL.Identity.Api/Startup.cs
@@ -1,3 +1,4 @@
+using CL.Identity.api.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -22,30 +23,38 @@ namespace CL.Identity.Api
 
 		public IConfiguration Configuration { get; }
 
+		public Startup(IHostEnvironment hostEnvironment)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(hostEnvironment.ContentRootPath)
+				.AddJsonFile("appsettings.json", true, true)
+				.AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+				.AddEnvironmentVariables();
+
+			if (hostEnvironment.IsDevelopment())
+			{
+				builder.AddUserSecrets<Startup>();
+			}
+
+			Configuration = builder.Build();
+		}
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllers();
+			services.AddIdentityConfiguration(Configuration);
+
+			services.AddApiConfiguration();
+
+			services.AddSwaggerConfiguration();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+			app.UseSwaggerConfiguration();
 
-			app.UseHttpsRedirection();
-
-			app.UseRouting();
-
-			app.UseAuthorization();
-
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllers();
-			});
+			app.UseApiConfiguration(env);
 		}
 	}
 }
